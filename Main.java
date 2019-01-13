8b0b9dbae577ef31a1f017ed79b0e04d79ec9d8e
Patrik Karlsson <patrik.karlsson@autocom.se>
2019-01-13 17:05:44 +0100

    Added StringBuilder

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 
-Class Main {
-
+class Main {
 	public static void main(String[] args) {
-
+		String org = "Hello Java";
+		String orgReverse = new StringBuilder(org).reverse().toString();
+		System.out.println(orgReverse);
 	}
-
 }
